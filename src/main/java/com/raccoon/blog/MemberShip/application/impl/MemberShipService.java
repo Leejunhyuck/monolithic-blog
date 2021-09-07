4ce24a290301d3ca4a49fcf94f5bf1135f753962
Leejunhyuck <hg00521@naver.com>
2021-09-08 01:06:46 +0900

    멤버십 구현

--- src/main/java/com/raccoon/blog/MemberShip/application/impl/MemberShipService.java
+++ src/main/java/com/raccoon/blog/MemberShip/application/impl/MemberShipService.java
@@ -1,4 +1,12 @@
 package com.raccoon.blog.MemberShip.application.impl;
 
+import com.raccoon.blog.Board.domain.Board;
+import com.raccoon.blog.Board.dto.BoardDto;
+import com.raccoon.blog.Board.dto.PageDto;
+import org.springframework.data.domain.Page;
+
 public interface MemberShipService {
+    public Board saveMemberShip(Board board);
+    public Page<Board> getMemberShipList(PageDto pageDto);
+    public void deleteMemberShip(BoardDto boardDto);
 }
--- src/main/java/com/raccoon/blog/MemberShip/application/impl/MemberShipServiceImpl.java
+++ src/main/java/com/raccoon/blog/MemberShip/application/impl/MemberShipServiceImpl.java
@@ -1,7 +1,26 @@
 package com.raccoon.blog.MemberShip.application.impl;
 
+import com.raccoon.blog.Board.domain.Board;
+import com.raccoon.blog.Board.dto.BoardDto;
+import com.raccoon.blog.Board.dto.PageDto;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberShipServiceImpl implements MemberShipService{
+
+    @Override
+    public Board saveMemberShip(Board board) {
+        return null;
+    }
+
+    @Override
+    public Page<Board> getMemberShipList(PageDto pageDto) {
+        return null;
+    }
+
+    @Override
+    public void deleteMemberShip(BoardDto boardDto) {
+
+    }
 }
